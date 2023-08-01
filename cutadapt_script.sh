d267cf4a774ae514c940b5e732f0e2b33d750475
wfenglund <72128748+wfenglund@users.noreply.github.com>
2023-08-01 14:21:29 +0200

    fixed so that the script points to the correct folders

--- cutadapt_script.sh
+++ cutadapt_script.sh
@@ -1,5 +1,5 @@
-for file in rawdata/*.fq.gz
+for file in Raw_data/*.fq.gz
 do
 	sample=$(basename -s .fq.gz "$file") 
-	cutadapt --discard-untrimmed -e 0.1 --no-indels -g ^file:primers.fa -o filtered2/{name}_$sample.fastq.gz $file
+	cutadapt --discard-untrimmed -e 0.1 --no-indels -g ^file:primers.fa -o Filtered_data/{name}_$sample.fastq.gz $file
 done
