7a80b4ed342e320404a850007fcd2a9c95533eaa
wfenglund <w.f.englund@gmail.com>
2024-08-30 18:42:25 +0200

    reworked project structure to incorporate merging of read pairs

--- cutadapt_script.sh
+++ cutadapt_script.sh
@@ -1,5 +1,9 @@
-for file in Raw_data/*.fq.gz
+# Specify primer file:
+primer="Primers/primer_file.fa"
+
+# Trim primers and create a file for each locus:
+for file in Merged_data/*.fq.gz
 do
-	sample=$(basename -s .fq.gz "$file") 
-	cutadapt --discard-untrimmed -e 0.1 --no-indels -g ^file:primers.fa -o Filtered_data/{name}_$sample.fastq.gz $file
+	sample=$(basename -s .fq.gz "$file")
+	cutadapt --discard-untrimmed -e 0.1 --no-indels -g ^file:$primer -o Filtered_data/{name}_$sample.fastq.gz $file
 done
