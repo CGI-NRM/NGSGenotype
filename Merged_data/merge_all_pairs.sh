60744cc9b90ab1234c17888ecb3fc71a2214ae7d
wfenglund <w.f.englund@gmail.com>
2024-11-28 19:19:00 +0100

    get rid of extra underscore from sample names

--- Merged_data/merge_all_pairs.sh
+++ Merged_data/merge_all_pairs.sh
@@ -9,5 +9,5 @@ do
   merged_out=${forward_in/"$raw_dir/"/""}
   output=${merged_out/"$fwd_end"/""}
   echo $output
-  ~/tools/BBTools/bbmap/bbmerge.sh in1=$forward_in in2=$reverse_in out=$output"_merged.fq.gz" outu1=$unm_dir"/"$output"_unmerged_1.fq.gz" outu2=$unm_dir"/"$output"_unmerged_2.fq.gz"
+  ~/tools/BBTools/bbmap/bbmerge.sh in1=$forward_in in2=$reverse_in out=$output".merged.fq.gz" outu1=$unm_dir"/"$output".unmerged_1.fq.gz" outu2=$unm_dir"/"$output".unmerged_2.fq.gz"
 done
