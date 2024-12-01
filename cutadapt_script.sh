6a6fa7e06d43086aba34a5a2132b6c3f5f1dea99
wfenglund <w.f.englund@gmail.com>
2024-12-01 20:13:00 +0100

    rework cutadapt script to take command line arguments

--- cutadapt_script.sh
+++ cutadapt_script.sh
@@ -1,9 +1,24 @@
+# Specify data:
+data_type=$1 # the first terminal argument will be taken as data type (single or merged)
+if [[ $data_type == "single" ]] # if single ended data
+then
+  dataset="Raw_data/*.fastq.gz"
+elif [[ $data_type == "merged" ]] # if merged data
+then
+  dataset=""Merged_data/*.merged.fq.gz""
+fi
+
 # Specify primer file:
-primer=$1 # run script with the primer file as an argument
+primers=$2 # the second terminal argument will be taken as primer file path
 
 # Trim primers and create a file for each locus:
-for file in Raw_data/*.fastq.gz
-do
-	sample=$(basename -s .fastq.gz "$file")
-	cutadapt --discard-untrimmed -e 0.1 --no-indels --error-rate 0.1 -a file:$primer -o Filtered_data/{name}_$sample.fastq.gz $file
-done
+trim_primers ()
+{
+  for file in $2
+  do
+    sample=$(basename -s .fastq.gz "$file")
+    cutadapt --discard-untrimmed -e 0.1 --no-indels --error-rate 0.1 -a file:$1 -o Filtered_data/{name}_$sample.fastq.gz $file
+  done
+}
+
+trim_primers $primers "$dataset" | tee cutadapt_log.out
