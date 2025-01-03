aa22937000cbe1ee769dacdbbef68b27de834ce2
wfenglund <w.f.englund@gmail.com>
2025-01-04 00:29:00 +0100

    fixed so that bbmerge output is actually written to log

--- Merged_data/merge_all_pairs.sh
+++ Merged_data/merge_all_pairs.sh
@@ -3,11 +3,17 @@ unm_dir="../Unmerged_reads"
 fwd_end="_1.fq.gz"
 rev_end="_2.fq.gz"
 
-for forward_in in $raw_dir/*$fwd_end
-do
-  reverse_in=${forward_in/"$fwd_end"/"$rev_end"}
-  merged_out=${forward_in/"$raw_dir/"/""}
-  output=${merged_out/"$fwd_end"/""}
-  echo $output
-  ~/tools/BBTools/bbmap/bbmerge.sh in1=$forward_in in2=$reverse_in out=$output".merged.fq.gz" outu1=$unm_dir"/"$output".unmerged_1.fq.gz" outu2=$unm_dir"/"$output".unmerged_2.fq.gz" | tee bbmerge_log.out
-done
+merge_all ()
+{
+  for forward_in in $raw_dir/*$fwd_end
+  do
+    reverse_in=${forward_in/"$fwd_end"/"$rev_end"}
+    merged_out=${forward_in/"$raw_dir/"/""}
+    output=${merged_out/"$fwd_end"/""}
+    echo $output
+    ~/tools/BBTools/bbmap/bbmerge.sh in1=$forward_in in2=$reverse_in out=$output".merged.fq.gz" outu1=$unm_dir"/"$output".unmerged_1.fq.gz" outu2=$unm_dir"/"$output".unmerged_2.fq.gz"
+  done
+}
+
+# Write stout and sterr to log file:
+merge_all 2>&1 | tee bbmerge_log.out
