a08d2803214a604e66e8e52ddad2be23a817043f
wfenglund <w.f.englund@gmail.com>
2025-03-12 20:47:00 +0100

    added log saving to filter_out_SNPs script

--- filter_out_SNPs.sh
+++ filter_out_SNPs.sh
@@ -1,11 +1,15 @@
 mkdir -p Filtered_data/SNP_filtered
 
-while read locus
-do
-  read sequence
-  locus_name=${locus/'>'/''}
-  for fastq in Filtered_data/*$locus_name*
+filter_snps() {
+  while read locus
   do
-    cutadapt -j 0 --discard-untrimmed -e 0.1 --no-indels --error-rate 0.1 --maximum-length 1 -a $sequence -o ${fastq/'Filtered_data/'/'Filtered_data/SNP_filtered/'} $fastq
-  done
-done < Primers/Bear_SNP_surrounding_bases.fa
+    read sequence
+    locus_name=${locus/'>'/''}
+    for fastq in Filtered_data/*$locus_name*
+    do
+      cutadapt -j 0 --discard-untrimmed -e 0.1 --no-indels --error-rate 0.1 --maximum-length 1 -a $sequence -o ${fastq/'Filtered_data/'/'Filtered_data/SNP_filtered/'} $fastq
+    done
+  done < Primers/Bear_SNP_surrounding_bases.fa
+}
+
+filter_snps | tee cutadapt_snp_trim_log.out
