e3a43050eb1ebf681f9ea87bef0f77cc04eab494
wfenglund <w.f.englund@gmail.com>
2025-06-08 17:54:00 +0200

    added script for running snpotypewriter on all samples at once

---
+++ Genotyped_SNPs/single_run.sh
@@ -0,0 +1,2 @@
+# Script for running snpotypewriter on all samples in the SNP_filtered folder (i.e. no chunks, but all in one run).
+python ./snpotypewriter.py ../Filtered_data/SNP_filtered > "all_genotypes.csv"
