d381a1c2a9e4272123e58edee59999f801f53bb3
wfenglund <w.f.englund@gmail.com>
2025-06-19 13:04:00 +0200

    updated SNP filtering script with chunk additions

--- filter_out_SNPs.sh
+++ filter_out_SNPs.sh
@@ -1,15 +1,19 @@
-mkdir -p Filtered_data/SNP_filtered
+# Usage:
+# bash filter_out_SNPs.sh
+# bash filter_out_SNPs.sh chunk_[n]
+
+mkdir -p Filtered_data/$1/SNP_filtered
 
 filter_snps() {
   while read locus
   do
     read sequence
     locus_name=${locus/'>'/''}
-    for fastq in Filtered_data/*$locus_name*
+    for fastq in Filtered_data/$1/*$locus_name*
     do
-      cutadapt -j 0 --discard-untrimmed -e 0.1 --no-indels --error-rate 0.1 --maximum-length 1 -a $sequence -o ${fastq/'Filtered_data/'/'Filtered_data/SNP_filtered/'} $fastq
+      cutadapt -j 0 --discard-untrimmed -e 0.1 --no-indels --error-rate 0.1 --maximum-length 1 -a $sequence -o ${fastq/'Filtered_data/'$1/'Filtered_data/'$1'/SNP_filtered/'} $fastq
     done
-  done < Primers/Bear_SNP_surrounding_bases.fa
+  done < ../make_snp_primers/Bear_SNP_surrounding_bases_current_v1.fa
 }
 
-filter_snps | tee cutadapt_snp_trim_log.out
+filter_snps $1 | tee cutadapt_snp_trim_log_${1//'/'/''}.out
