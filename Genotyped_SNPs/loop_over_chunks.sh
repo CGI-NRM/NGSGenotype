55a6b2d8931a3ee84145f3a1d731246d07abf481
wfenglund <w.f.englund@gmail.com>
2025-06-21 19:53:00 +0200

    expanded script for genotyping all chunks

--- Genotyped_SNPs/loop_over_chunks.sh
+++ Genotyped_SNPs/loop_over_chunks.sh
@@ -1,12 +1,19 @@
 # Usage (note, it assumes that subfolders are prefixed with "chunk_"):
-# bash loop_over_chunks.sh [folder containing chunk folders]
+# bash loop_over_chunks.sh ../Filtered_data/
 
-for cur_folder in $1/chunk_*
+for cur_folder in $1/chunk*
 do
   chunk_name=${cur_folder/*\/}
   cur_time=`python -c "import datetime; cur_time = datetime.datetime.now(); print(f'{cur_time.hour}:{cur_time.minute}:{cur_time.second}')"`
   echo "Parsing "$chunk_name". ("$cur_time")"
-  python ./snpotypewriter.py $cur_folder > $chunk_name"_genotypes.csv"
+  snp_folder=$cur_folder"/SNP_filtered/"
+  if [[ -d $snp_folder ]]
+  then
+    echo " - Genotyping files in "$snp_folder"."
+    python ./snpotypewriter.py $snp_folder > $chunk_name"_genotypes.csv"
+  else
+    echo " - No SNP folder found."
+  fi
   echo " - Done."
 done
 cur_time=`python -c "import datetime; cur_time = datetime.datetime.now(); print(f'{cur_time.hour}:{cur_time.minute}:{cur_time.second}')"`
